70d74786ab1ac00a9ddf8e8dfe46f298a1afc9b7
Vanessa Mendez <182719762+mendezpvi@users.noreply.github.com>
2024-11-07 21:58:26 -0500

    fix: image paths

--- js/posts.js
+++ js/posts.js
@@ -1,48 +1,48 @@
 export const posts = [
     {
-        image: "../assets/images/image-01.webp",
+        image: "https://raw.githubusercontent.com/mendezpvi/fcp-learning-journal/refs/heads/main/assets/images/image-01.webp",
         date: "November 06, 2024",
         title: "My new journey as a bootcamp student.",
         content: "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
         url: "pages/post-01.html"
     },
     {
-        image: "../assets/images/image-02.webp",
+        image: "https://raw.githubusercontent.com/mendezpvi/fcp-learning-journal/refs/heads/main/assets/images/image-02.webp",
         date: "October 06, 2024",
         title: "Blog one",
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         url: "#"
     },
     {
-        image: "../assets/images/image-03.webp",
+        image: "https://raw.githubusercontent.com/mendezpvi/fcp-learning-journal/refs/heads/main/assets/images/image-03.webp",
         date: "September 06, 2024",
         title: "Blog two",
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         url: "#"
     },
     {
-        image: "../assets/images/image-04.webp",
+        image: "https://raw.githubusercontent.com/mendezpvi/fcp-learning-journal/refs/heads/main/assets/images/image-04.webp",
         date: "July 06, 2024",
         title: "Blog three",
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         url: "#"
     },
     {
-        image: "../assets/images/image-05.webp",
+        image: "https://raw.githubusercontent.com/mendezpvi/fcp-learning-journal/refs/heads/main/assets/images/image-05.webp",
         date: "July 06, 2024",
         title: "Blog four",
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         url: "#"
     },
     {
-        image: "../assets/images/image-06.webp",
+        image: "https://raw.githubusercontent.com/mendezpvi/fcp-learning-journal/refs/heads/main/assets/images/image-06.webp",
         date: "July 06, 2024",
         title: "Blog five",
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         url: "#"
     },
     {
-        image: "../assets/images/image-07.webp",
+        image: "https://raw.githubusercontent.com/mendezpvi/fcp-learning-journal/refs/heads/main/assets/images/image-07.webp",
         date: "July 06, 2024",
         title: "Blog six",
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
